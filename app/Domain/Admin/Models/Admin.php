b266c79a3f9bf66eda575e8bbf1e2dd7b7b9ec3b
hoang van hung <hungkio16.9.98@gmail.com>
2023-06-08 16:37:00 +0700

    remote level case

--- app/Domain/Admin/Models/Admin.php
+++ app/Domain/Admin/Models/Admin.php
@@ -50,14 +50,13 @@ class Admin extends Authenticatable implements HasMedia
     ];
 
     const LEVEL = [
-        'SE_PR', 'Se_DE', 'SE', 'SE_DE', 'SE_FL', 'SE_TW', 'DTD', 'AV', 'DBAV', 'PRT', 'Basic_rt', 'FL', 'FLB', 'FLP',
+        'SE_PR', 'SE', 'SE_DE', 'SE_FL', 'SE_TW', 'DTD', 'AV', 'DBAV', 'PRT', 'Basic_rt', 'FL', 'FLB', 'FLP',
         'VT', 'VTP', 'VTP360', 'HEN', 'INV', 'Floor', 'EXT', 'PANO', 'HI_END'
     ];
 
     const ESTIMATE = [
         'INV' => 1,
         'SE' => 3,
-        'Se_DE' => 3,
         'SE_DE' => 3,
         'SE_PR' => 5,
         'SE_FL' => 6,
@@ -83,7 +82,6 @@ class Admin extends Authenticatable implements HasMedia
     const ESTIMATE_QA = [
         'INV' => 0.5,
         'SE' => 1.5,
-        'Se_DE' => 1.5,
         'SE_DE' => 1.5,
         'SE_PR' => 2.5,
         'SE_FL' => 3,
@@ -144,7 +142,6 @@ class Admin extends Authenticatable implements HasMedia
     const COST = [
         'INV' => [2000,400,0],
         'SE' => [3500,700,8000],
-        'Se_DE' => [3500,700,8000],
         'SE_DE' => [3500,700,8000],
         'SE_PR' => [4500,900,10000],
         'SE_FL' => [4500,900,10000],
