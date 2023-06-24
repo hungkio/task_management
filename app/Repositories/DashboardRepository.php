6e42961a0073e50ce3959b9408c8984153234d38
DESKTOP-5M46O0H\hung <hungkio16.9.98@gmail.com>
2023-06-24 20:25:25 +0700

    no message

---
+++ app/Repositories/DashboardRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Tasks;
+use Carbon\Carbon;
+
+class DashboardRepository extends BaseRepository
+{
+    public function getModel()
+    {
+        return Tasks::class;
+    }
+
+    public function getTasks($status, $condition = [])
+    {
+        return $this->_model->where('status', $status)->where($condition)->orderBy('updated_at', 'DESC')->get();
+    }
+
+    public function getTodayTasks($status, $condition = [])
+    {
+        return $this->_model->whereDate('created_at', Carbon::today())->where('status', $status)->where($condition)->orderBy('updated_at', 'DESC')->get();
+    }
+
+}
