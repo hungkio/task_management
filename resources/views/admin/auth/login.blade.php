ec0e36c4c02c55803717a66645768904edf929b7
Pike-the-dev <nghiavt@arrowhitech.biz>
2023-09-22 23:38:31 +0700

    style login form

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,7 +1,84 @@
 @extends('admin.layouts.base')
 
-@section('title', __('Đăng nhập'))
+@section('title', __('Login'))
+@push('css')
+  <style>
+    .page-content{
+        background-image: url('{{ asset('images/background.jpg') }}');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center center;
+    }
+    .login-form{
+        max-width: 600px;
+        width: 100%;
+        height: 80%;
+    }
+    .login-form .card{
+        background: linear-gradient(to bottom, rgba(103,28,12, 0.8) 7%, rgba(189,77,41,0.6) 30%, rgba(247,139,74,0.4) 80%);
+        border: solid 2px #8e6efa;
+    }
+    .logo{
+        max-width: 150px;
+        width: 100%;
+        margin: 0 auto;
+        display: flex;
+    }
+    .logo img{
+        width: 100%;
+        object-fit: cover;
+    }
+    .card-body{
+        padding: 0 75px;
+    }
+    .login-form input[type="text"],
+    .login-form input[type="password"]{
+        background-color: rgba(255, 255, 255, 0.5);
+        border: none;
+        height: 60px;
+        border-radius: 6px;
+        font-size: 20px;
+        font-weight: 700;
+    }
+    .login-form i{
+        color: #000 !important;
+        font-size: 20px;
+    }
+    .login-form input::placeholder{
+        color: #fff;
+        font-weight: 700;
+        font-size: 20px;
+    }
+    .form-control-feedback{
+        top: 13px;
+    }
+    .login-text{
+        font-size: 20px;
+        font-weight: 1000;
+        color: #fff;
+        text-transform: uppercase;
+    }
+    .btn-login {
+        border: solid 2px #fff;
+        border-radius: 8px;
+        background-image: linear-gradient(to right, red , yellow);
+        transition: 0.5s all;
+        background-size: 200% 100%;
+        background-position: 50% 0;
+        width: 100%;
+        padding: 10px 0;
+        outline: none;
+    }
 
+    .btn-login:hover {
+        background-position: 100% 100%;
+        transition: 0.5s all;
+    }
+    .btn-login:focus{
+        outline: none;
+    }
+  </style>
+@endpush
 @section('content')
     <div class="page-content">
 
@@ -14,10 +91,13 @@
                 <!-- Login card -->
                 <form class="login-form" action="{{ route('admin.login') }}" method="POST" id="login-form" data-block>
                     @csrf
-                    <div class="card mb-0">
-                        <div class="card-body">
+                    <div class="card mb-0 h-100">
+                        <div class="card-body h-100">
+                            <div class="logo">
+                                <img src="{{ asset(('images/logo-login.png')) }}" alt="">
+                            </div>
                             <div class="text-center mb-5 mt-3">
-                                <h3 class="mb-0">{{ __('Đăng nhập') }}</h3>
+                                <h3 class="mb-0 login-text">{{ __('Login') }}</h3>
                             </div>
 
                             <div class="form-group form-group-feedback form-group-feedback-left">
@@ -33,7 +113,7 @@
                             </div>
 
                             <div class="form-group form-group-feedback form-group-feedback-left">
-                                <input id="password" name="password" type="password" class="form-control @error('password') border-danger @enderror" placeholder="{{ __('Mật khẩu') }}">
+                                <input id="password" name="password" type="password" class="form-control @error('password') border-danger @enderror" placeholder="{{ __('Password') }}">
                                 <div class="form-control-feedback">
                                     <i class="far fa-lock text-muted"></i>
                                 </div>
@@ -43,19 +123,9 @@
                                 </span>
                                 @enderror
                             </div>
-
-                            <div class="form-group d-flex align-items-center">
-                                <div class="form-check mb-0">
-                                    <label class="form-check-label">
-                                        <input type="checkbox" name="remember" class="form-input-styled" data-fouc>
-                                        {{ __('Ghi nhớ') }}
-                                    </label>
-                                </div>
-                            </div>
-
                             <div class="form-group">
-                                <button class="btn btn-primary btn-block">
-                                    {{ __('Đăng nhập') }}
+                                <button class="login-text btn-login">
+                                    {{ __('Login') }}
                                 </button>
                             </div>
                         </div>
